2e9fcaa93c36daf6a728d2100e8429d4443cee09
KimSong2 <ssongyi0325@naver.com>
2023-08-19 18:42:59 +0900

    first

---
+++ src/main/java/com/project/pigable/PigableApplication.java
@@ -0,0 +1,13 @@
+package com.project.pigable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigableApplication.class, args);
+	}
+
+}
